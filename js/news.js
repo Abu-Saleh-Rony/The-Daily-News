69be318ba306d665c3701bc18bb7bccaa28f3609
abusalehrony <abusalehrony12@gmail.com>
2022-09-03 18:40:36 +0600

    added Dynamic News Portal

--- js/news.js
+++ js/news.js
@@ -18,4 +18,37 @@ const displayCategory = categories => {
     });
 }
 
+const loadnewsdetails = async () => {
+    const url = `https://openapi.programming-hero.com/api/news/category/01`
+    const res = await fetch(url);
+    const data = await res.json();
+    displaynews(data.data);
+}
+
+const displaynews = portalnews => {
+    const newsContainer = document.getElementById('news-portal');
+    portalnews.forEach(news => {
+        const newsDiv = document.createElement('div');
+        newsDiv.setAttribute('class', 'row gy-5')
+        newsDiv.innerHTML = ` 
+        <div class="col-md-4">
+        <img src="${news.image_url}" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+        <div class="card-body">
+            <h5 class="card-title">${news.title}</h5>
+            <p class="card-text">${news.details}</p>
+            <p class="card-text"><small class="text-muted">${news.author.name}</small></p>
+            <p class="card-text"><small class="text-muted">${news.author.published_date}</small></p>
+        </div>
+    </div>
+        `
+        newsContainer.appendChild(newsDiv);
+    })
+
+}
+
+loadnewsdetails();
+
+
 loadCategories();
