166495d40789a01ad5a991e604b5607d685498fe
abusalehrony <abusalehrony12@gmail.com>
2022-09-03 22:28:14 +0600

    added description doted

--- js/news.js
+++ js/news.js
@@ -31,7 +31,8 @@ const displaynews = portalnews => {
     portalnews.forEach(news => {
 
         const portalElement = document.getElementById('category-news-found');
-        portalElement.innerHTML = ` <p>${portalnews.length} Items found for this Category.</p> `
+        portalElement.innerHTML = ` <p>
+        ${portalnews.length} Items found for this Category.</p> `
 
         const newsDiv = document.createElement('div');
         newsDiv.setAttribute('class', 'row')
@@ -42,42 +43,42 @@ const displaynews = portalnews => {
     <div class="col-md-8">
         <div class="card-body">
             <h5 class="card-title">${news.title}</h5>
-            <p class="card-text">${news.details.slice(0, 200)}</p>
+            <p class="card-text">${news.details.slice(0, 200)}...</p>
 
 
-            <div class ="d-flex align-items-center justify-content-between">
+    <div class="d-flex align-items-center justify-content-between">
 
-            <div class ="d-flex align-items-center">
+        <div class="d-flex align-items-center">
 
             <div>
-            <img src="${news.author.img}"
-            alt="Generic placeholder image" class="img-fluid rounded-circle border border-dark border-3"
-            style="width: 70px;">
+                <img src="${news.author.img}"
+                    alt="Generic placeholder image" class="img-fluid rounded-circle border border-dark border-3"
+                    style="width: 70px;">
             </div>
 
             <div>
-            <p class="card-text"><small class="text-muted">${news.author.name}</small></p>
-            <p class="card-text"><small class="text-muted">${news.author.published_date}</small></p>
+                <p class="card-text"><small class="text-muted">${news.author.name}</small></p>
+                <p class="card-text"><small class="text-muted">${news.author.published_date}</small></p>
             </div>
 
-           
-            </div>
 
-            <div>
+        </div>
+
+        <div>
             <i class="fa-solid fa-eye">${news.total_view}</i>
-            </div>
+        </div>
 
-            <div>
+        <div>
             <button type="button" class="btn btn-primary">Details News</button>
-            </div>
+        </div>
 
 
-          </div
+    </div
            
             
-        </div>
-    </div>
-        `
+        </div >
+    </div >
+    `
         newsContainer.appendChild(newsDiv);
     })
 
